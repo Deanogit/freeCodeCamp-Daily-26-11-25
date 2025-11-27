e73299e6a2d7904e65cd9ac2e368f0bf43471868
Dean Howe <hideanhowe@gmail.com>
2025-11-27 15:27:37 +0000

    fb

--- index.js
+++ index.js
@@ -6,3 +6,21 @@
 // Numbers that are multiples of both 3 and 5 are replaced with "FizzBuzz"
 // All other numbers remain as integers in ascending order, starting from 1.
 // The array must start at 1 and have no missing or extra elements.
+
+// lets write fizzbuzz again
+
+function fizzBuzz(e) {
+  const fbArray = [];
+  for (let i = 1; i >= e; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      fbArray.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      fbArray.push('Fizz');
+    } else if (i % 5 === 0) {
+      fbArray.push('Buzz');
+    } else {
+      fbArray.push(i);
+    }
+  }
+  return fbArray;
+}
